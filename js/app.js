efebf8b293669422ed466799d3c3b4f6827d3a52
improvola <83605546+improvola@users.noreply.github.com>
2021-05-29 23:26:17 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 400px
+			  breakpoint: 200,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 1,
+				slidesToScroll: 1
+			  }
+			},{
+			  // screens greater than >= 500px
+			  breakpoint: 500,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			  }
+			}
+		]
+	});
+});
+
